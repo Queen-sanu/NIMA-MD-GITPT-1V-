a2a5f158956f60eb725d733cf6de2870c90b6acb
DORA-ID <prarthanashashini93@gmail.com>
2025-08-09 15:41:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ඔයාගෙ session id එක දාන්න මෙතෙන්ට",
+SESSION_ID: process.env.SESSION_ID || "NIMA~MD&AalnxaaI#kgn562jH2F5YpXaxHXiu00wTV24zikSm91qAYgSeFYw",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
